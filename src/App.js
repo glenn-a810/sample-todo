66ff5a3e3d57a105344777a9f7e4d4767dad04ba
Glenn <a810.glenn@gmail.com>
2022-11-02 21:42:00 +0900

    vanila js 비교

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css'
 
 function App() {
+  // vanila js
+  const para = document.createElement('p')
+  para.textContent = 'This is invisible'
+  document.getElementById('root').append(para)
+
+  // react.js
   return (
     <div className="App">
       <h2>npm run start</h2>
+      <p>This is also visible</p>
     </div>
   )
 }
