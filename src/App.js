5c43778e516a4c11d40cc44f55923240d75feba9
Glenn <a810.glenn@gmail.com>
2022-11-05 20:14:40 +0900

    styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css'
+// import './App.css'
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
   return (
     <div className="App">
       <h2>npm run start</h2>
-      <p>This is also visible</p>
       <ExpenseItem />
     </div>
   )
