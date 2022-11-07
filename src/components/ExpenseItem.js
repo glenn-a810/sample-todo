088fad4c9345aeb9a558c199fa411bab44d2b056
Glenn <a810.glenn@gmail.com>
2022-11-07 20:24:27 +0900

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,19 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 10, 7)
-  const expenseTitle = 'Car Insurance'
-  const expenseAmount = 294.67
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2022, 10, 7)
+  // const expenseTitle = 'Car Insurance'
+  // const expenseAmount = 294.67
 
   return (
     <div className="expense-item">
       {/*<div>March 28th 2022</div>*/}
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
         {/*<h2>Car Insurance</h2>*/}
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
         {/*<div className="expense-item__price">$294.67</div>*/}
-        <div className="expense-item__price">${expenseAmount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   )
